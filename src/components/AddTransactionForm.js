1e383dcd916b8bfca5ef4341b969f525bb7dfbb8
ESTON NDUNGU <estonndungu1@gmail.com>
2024-08-12 14:10:02 +0300

    add validtion to the inputs

--- src/components/AddTransactionForm.js
+++ src/components/AddTransactionForm.js
@@ -43,10 +43,10 @@ function AddTransactionForm() {
     <div className="ui segment">
       <form className="ui form" onSubmit={handleSubmit}>
         <div className="inline fields">
-          <input type="date" name="date" value={date} onChange={(e) => setDate(e.target.value)} />
-          <input type="text" name="description" value={description} onChange={(e) => setDescription(e.target.value)} placeholder="Description" />
-          <input type="text" name="category"value={category} onChange={(e) => setCategory(e.target.value)}placeholder="Category" />
-          <input type="number" name="number" value={amount} onChange={(e) => setAmount(e.target.value)} placeholder="Amount" step="0.01" />
+          <input type="date" name="date"   value={date} onChange={(e) => setDate(e.target.value)} required/>
+          <input type="text" name="description" value={description} onChange={(e) => setDescription(e.target.value)} placeholder="Description" required/>
+          <input type="text" name="category"value={category} onChange={(e) => setCategory(e.target.value)}placeholder="Category" required/>
+          <input type="number" name="number" value={amount} onChange={(e) => setAmount(e.target.value)} placeholder="Amount" step="0.01" required />
         </div>
         <button className="ui button" type="submit">
           Add Transaction
